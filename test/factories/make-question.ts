3cf96b475907d81149a2ed631d4e32d235aa2ecd
FeAranha <felipearanha.c@gmail.com>
2023-10-02 16:24:40 -0300

    gerando dados ficticios | use-case delete anwser

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,14 +3,19 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
   const question = Question.create({
     authorId: new UniqueEntityID(),
-    title: 'Example',
-    content: 'Example content',
+    title: faker.lorem.sentence(),
+    content: faker.lorem.text(),
     ...override,
-  })
+  },
+    id,
+  )
 
   return question
 }
