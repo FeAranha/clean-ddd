b41e53eaf3f70e83a9171fd22760cc1a9de77eb2
FeAranha <felipearanha.c@gmail.com>
2023-10-06 12:34:14 -0300

    refatorando testes unitarios

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,18 +1,21 @@
-import { Either, left, right } from "@/core/either"
-import { Question } from "../../enterprise/entities/question"
-import { QuestionsRepository } from "../repositories/questions-repository"
-import { ResourceNotFoundError } from "./errors/resource-not-found-error"
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Either, left, right } from '@/core/either'
+import { ResourceNotFoundError } from '@/domain/forum/application/use-cases/errors/resource-not-found-error'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
-type GetQuestionBySlugUseCaseResponse = Either<ResourceNotFoundError, {
-  question: Question
-}>
+type GetQuestionBySlugUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    question: Question
+  }
+>
 
 export class GetQuestionBySlugUseCase {
-  constructor(private questionsRepository: QuestionsRepository) { }
+  constructor(private questionsRepository: QuestionsRepository) {}
 
   async execute({
     slug,
